8abe449bd4e9c8bb415f5082f61e75726d79e363
ERICyuone <46913072+ERICyuone@users.noreply.github.com>
2019-07-05 18:40:22 +0800

    后端发布接口
    完成单因素的读取，判断后回写到数据库；
    完成单因素异常库的写入以及火焰异常因素的数据更新；

---
+++ publish.bat
@@ -0,0 +1,24 @@
+wsimport -keep http://localhost:8080/server/AGVDetData?wsdl
+wsimport -keep http://localhost:8080/server/AutoEquipPersonIllegDetData?wsdl
+wsimport -keep http://localhost:8080/server/DustDensityDetData?wsdl
+wsimport -keep http://localhost:8080/server/EquipEarthDetData?wsdl
+wsimport -keep http://localhost:8080/server/FireDetData?wsdl
+wsimport -keep http://localhost:8080/server/FirerQuantityDetData?wsdl
+wsimport -keep http://localhost:8080/server/GasDensityDetData?wsdl
+wsimport -keep http://localhost:8080/server/HumidityDetData?wsdl
+wsimport -keep http://localhost:8080/server/LiftingPersonIllegDetData?wsdl
+wsimport -keep http://localhost:8080/server/PersonClothesDetData?wsdl
+wsimport -keep http://localhost:8080/server/RoomOverloadDetData?wsdl
+wsimport -keep http://localhost:8080/server/TempDetData?wsdl
+wsimport -keep http://localhost:8080/server/TestPersonIllegDetData?wsdl
+wsimport -keep http://localhost:8080/server/FireAndDustDensityDetAlarm?wsdl
+wsimport -keep http://localhost:8080/server/FireAndFirerQuantityDetAlarm?wsdl
+wsimport -keep http://localhost:8080/server/FireAndGasDensityDetAlarm?wsdl
+wsimport -keep http://localhost:8080/server/TempAndFirerQuantityDetAlarm?wsdl
+wsimport -keep http://localhost:8080/server/TempAndGasDensityDetAlarm?wsdl
+wsimport -keep http://localhost:8080/server/ComprehScore?wsdl
+wsimport -keep http://localhost:8080/server/StopErrorInsert?wsdl
+wsimport -keep http://localhost:8080/server/FireDetDataError?wsdl
+exit
+
+
